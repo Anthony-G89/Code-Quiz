2feb83e502730ae9a433049233951b83174bf764
Anthony Guerrero <adguerrero1935@gmail.com>
2020-01-21 17:08:43 -0500

    added some tags to the html and style to the css

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+var startButton= document.getElementById('start-button');
+var questionContaioner= document.getElementById('question-container');
+var timer= document.getElementById('#count-down');
 
+
+
+startButton.addEventListener("click",startGame)
+
+function startGame(){
+startButton.classList.add('hide');
+questionContaioner.classList.remove('hide');
+
+
+}
+
+function timer(){
+    setInterval
+}
+
+function setNextQuestion(){
+
+}
+
+
+function selectAnswer(){
+
+}
