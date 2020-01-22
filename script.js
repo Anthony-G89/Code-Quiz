bd0af4d8d72aafcb8fcd2296b125999fa597cdbf
Anthony Guerrero <adguerrero1935@gmail.com>
2020-01-21 20:31:52 -0500

    added a timer

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var startButton= document.getElementById('start-button');
-var questionContaioner= document.getElementById('question-container');
-var timer= document.getElementById('#count-down');
+var startButton= document.querySelector('#start-button');
+var questionContaioner= document.querySelector('#question-container');
+var timer= document.querySelector('#count-down');
+var count= 10;
 
 
 
@@ -9,12 +10,24 @@ startButton.addEventListener("click",startGame)
 function startGame(){
 startButton.classList.add('hide');
 questionContaioner.classList.remove('hide');
+ interval= setInterval(function(){
+     count -= 1;
+     timer.textContent= ("timer: " + count);
+
+     if(count === 0){
+         clearInterval("Time is up");
+     };
+    
+    
+},1000);
 
 
 }
 
 function timer(){
-    setInterval
+    if(count === 0){
+        clearInterval(count);
+    }
 }
 
 function setNextQuestion(){
