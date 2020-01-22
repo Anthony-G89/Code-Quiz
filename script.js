73aa6dd5349ebeb4bab3b61553c6f4bf09488afd
Anthony Guerrero <adguerrero1935@gmail.com>
2020-01-22 01:55:29 -0500

    added a title img and some more style with CSS

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var startButton= document.querySelector('#start-button');
 var questionContaioner= document.querySelector('#question-container');
 var timer= document.querySelector('#count-down');
 var nextBtn= document.querySelector('#next-button');
+var introParagraph= document.querySelector('#intro');
+var header= document.querySelector('#main');
 var count= 10;
 
 
@@ -14,6 +16,7 @@ startButton.classList.add('hide');
 questionContaioner.classList.remove('hide');
 timer.classList.remove ('hide');
 nextBtn.classList.remove('hide');
+introParagraph.classList.add('hide');
 
  interval= setInterval(function(){
      count -= 1;
