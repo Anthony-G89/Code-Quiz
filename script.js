c047c696558001dd59575d8df7681054578bb816
Anthony Guerrero <adguerrero1935@gmail.com>
2020-01-26 19:22:20 -0500

    added a next button and fixed correct and wrong div's

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ var quiz = document.querySelector("#quiz");
 var questionsElement = document.querySelector("#questions");
 var choices = document.querySelector(".choices");
 var results = document.querySelector("#results");
-var choicesClass = document.querySelector(".choicesClass")
-
-
-
+var choicesClass = document.querySelector(".choicesClass");
+var correctElement = document.querySelector("#correct");
+var wrongElement = document.querySelector("#wrong");
+var nextElement = document.querySelector("#nextBtn");
+var correctOrWrong = "";
 var runningQuestion = 0;
 var count = 70;
+
 
 var questions = [
     {
@@ -29,20 +31,17 @@ var questions = [
     },
     {
         question: "What is the correct JavaScript syntax to change the contact of the HTML element below?",
-        choices: ["A. document.getElementById(\"demo\").innerHTML = Hello World!", "B. document.getElement(\"p\").innerHTML = Hello World!", "C. #demo.innerHTML = \"Hello World!;\"", "D. document.getElementByNamw(\"p\").innerHTML = Hello World!"],
+        choices: ["A. document.getElementById(\"demo\").innerHTML = Hello World!", "B. document.getElement(\"p\").innerHTML = Hello World!", "C. #demo.innerHTML = \"Hello World!;\"", "D.document.getElementByNamw(\"p\").innerHTML = Hello World!"],
         answer: "A. document.getElementById(\"demo\").innerHTML = Hello World!"
     },
     {
         question: "What is the correct HTML for referring to an external style sheet",
         choices: ["A. <stylesheet>mystyles.css</stylesheet>", "B. <style src= \"mystyle.css\">", "C. <link rel=\"stylesheet\" type=\"text/css\" href=\"mystyle.css\">", "D. <linkin rel=\"stylesheet\" type=\"text/css\" href=\"mystyle.css\">"],
-        answer: "C. <link rel=\"stylesheet\" type=\"text/css\" href=\"mystyle.css\""
+        answer: "C. <link rel=\"stylesheet\" type=\"text/css\" href=\"mystyle.css\">"
 
         
     }
 ]
-
-
-
 
 start.addEventListener("click",startGame)
 
@@ -54,15 +53,11 @@ function startGame(){
      if(count === 0){
          clearInterval(interval);
      };
-    
-
 },1000);
 
 start.style.display = 'none';
 
-
 renderQuestion();
-
 }
 
 function renderQuestion(){
@@ -70,64 +65,53 @@ function renderQuestion(){
     choices.innerHTML= "";
     questionsElement.innerHTML = "<p>" + q.question + "</p>";
     for(var i=0; i < q.choices.length; i++){
-        console.log(q.choices[i]);
-        
-        // choices.textContent =q.choices[i];
+
         var p = document.createElement("p");
         p.setAttribute('class',"choicesClass");
         p.textContent = q.choices[i];
-        choices.append(p)
-       
+        choices.append(p);
+
     }
 
-
-//     console.log(q.question);
-        
-//     if(q.choices[0] === q.answer ){
-//         alert("correct");
-//    } else("wrong");
-
-    
 quiz.style.display= 'block';
-   
-
-    
+       
 }
+
 
 document.addEventListener("click", function(){
     if(event.target.matches("p")){
         console.log(event.target.textContent);
         if(event.target.textContent === questions[runningQuestion].answer ){
-            // alert("correct");
-            results.textContent = "Correct"
-            results.style.display= 'block';
+            correctElement.textContent = "CORRECT"
+            correctElement.style.display= 'block';
+            
             runningQuestion++;
-
-            renderQuestion();
 
         } else {
-            results.textContent = "Wrong"
-            results.style.display= 'block';
+            wrongElement.textContent = "WRONG";
+            wrongElement.style.display= 'block';
             runningQuestion++;
-            // count = count - 6;
             count -= 6;
-            renderQuestion();
-
 
         };
 
     }
     
-})
+});
 
-function setNextQuestion(){
+nextElement.addEventListener("click", nextquestion)
 
+function nextquestion(){
+    renderQuestion();
+    document.querySelector("#correct").innerHTML = "";
+    document.querySelector("#wrong").innerHTML = "";
 }
 
-
-function selectAnswer(){
-
+function results(){
+  results.style.display="block";
+  renderQuestion();
 }
+
 
 
 
