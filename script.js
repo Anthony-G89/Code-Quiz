90a7e42a1020677095a535cb788eb585b49f3cac
Anthony Guerrero <adguerrero1935@gmail.com>
2020-08-12 13:32:43 -0400

    Fixed some styling on that start button and timer

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var correctElement = document.querySelector("#correct");
 var wrongElement = document.querySelector("#wrong");
 var nextElement = document.querySelector("#nextBtn");
 var runningQuestion = 0;
-var count = 40;
+var count = 90;
 var highScores = [];
 
 
@@ -17,12 +17,12 @@ var questions = [
     {
         question: "What does HTML stand for __________.",
         choices: ["A. Hypertext Markup Language", "B. Hyper Mark Language", "C. JSON", "D. Height"],
-         answer: "A. Hypertext Markup Language"
+        answer: "A. Hypertext Markup Language"
     },
     {
         question: "What does CSS stand for _____________.",
-         choices: ["A. Cascading Style Sheets", "B. Cascading Sheets Style", "C. Cascading Sheep style", "D. Computer Style Sheets"],
-         answer: "A. Cascading Style Sheets"
+        choices: ["A. Cascading Style Sheets", "B. Cascading Sheets Style", "C. Cascading Sheep style", "D. Computer Style Sheets"],
+        answer: "A. Cascading Style Sheets"
     },
     {
         question: "Choose the correct HTML element for the largest heading:",
@@ -39,106 +39,106 @@ var questions = [
         choices: ["A. <stylesheet>mystyles.css</stylesheet>", "B. <style src= \"mystyle.css\">", "C. <link rel=\"stylesheet\" type=\"text/css\" href=\"mystyle.css\">", "D. <linkin rel=\"stylesheet\" type=\"text/css\" href=\"mystyle.css\">"],
         answer: "C. <link rel=\"stylesheet\" type=\"text/css\" href=\"mystyle.css\">"
 
-        
+
     }
 ]
 
-start.addEventListener("click",startGame)
+start.addEventListener("click", startGame)
 
-function startGame(){
- interval= setInterval(function(){
-     count -= 1;
-     timer.textContent= ("TIMER: " + count);
+function startGame() {
+    interval = setInterval(function () {
+        count -= 1;
+        timer.textContent = ("TIMER: " + count);
 
-     if(count === 0){
-         clearInterval(interval);
-         score();
-     };
-    
-},1000);
+        if (count === 0) {
+            clearInterval(interval);
+            score();
+        };
 
-start.style.display = 'none';
+    }, 1000);
 
-renderQuestion();
+    start.style.display = 'none';
+
+    renderQuestion();
 }
 
-function renderQuestion(){
-    if(runningQuestion === questions.length){
+function renderQuestion() {
+    if (runningQuestion === questions.length) {
         clearInterval(interval);
         score();
         return;
-        
+
     }
 
     var q = questions[runningQuestion];
-    choices.innerHTML= "";
+    choices.innerHTML = "";
     questionsElement.innerHTML = "<p>" + q.question + "</p>";
-    for(var i=0; i < q.choices.length; i++){
+    for (var i = 0; i < q.choices.length; i++) {
 
         var p = document.createElement("p");
-        p.setAttribute('class',"choicesClass");
+        p.setAttribute('class', "choicesClass");
         p.textContent = q.choices[i];
         choices.append(p);
-    
+
 
     }
 
-quiz.style.display= 'block';
-       
+    quiz.style.display = 'block';
+
 }
 
 
-document.addEventListener("click", function(){
-    if(event.target.matches("p")){
+document.addEventListener("click", function () {
+    if (event.target.matches("p")) {
 
-        if(event.target.textContent === questions[runningQuestion].answer ){
+        if (event.target.textContent === questions[runningQuestion].answer) {
             correctElement.textContent = "CORRECT"
-            correctElement.style.display= 'block';
+            correctElement.style.display = 'block';
             runningQuestion++;
 
 
         } else {
             wrongElement.textContent = "WRONG";
-            wrongElement.style.display= 'block';
+            wrongElement.style.display = 'block';
             runningQuestion++;
             count -= 6;
 
         };
 
     }
-    
+
 });
 
 nextElement.addEventListener("click", nextquestion)
 
-function nextquestion(){
+function nextquestion() {
     renderQuestion();
     document.querySelector("#correct").innerHTML = "";
     document.querySelector("#wrong").innerHTML = "";
 }
 
 
-function score(){
+function score() {
 
     quiz.style.display = "none";
     results.style.display = "block";
-    
-    
+
+
 
 }
 
 
-document.querySelector("#submit").addEventListener("click", function(){
-        var initialsElm = document.querySelector("#initials");
-        var initials = initialsElm.value;
-        var score = count;
-        console.log(initials, score);
-        highScores.push({
-            initials: initials,
-            score: score
-        })
-        localStorage.setItem("highscores", JSON.stringify(highScores))
-        
+document.querySelector("#submit").addEventListener("click", function () {
+    var initialsElm = document.querySelector("#initials");
+    var initials = initialsElm.value;
+    var score = count;
+    console.log(initials, score);
+    highScores.push({
+        initials: initials,
+        score: score
+    })
+    localStorage.setItem("highscores", JSON.stringify(highScores))
+
 })
 
 
